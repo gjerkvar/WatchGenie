8bced8ddd5c0f441c05832adbf7897da8e6ff900
Gjermund <gjerkvar@gmail.com>
2025-02-10 21:54:38 +0100

    .

--- api/completions.js
+++ api/completions.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/', async (req, res) => {
+app.post('/api/completions', async (req, res) => {
   const { prompt } = req.body;
 
   try {
